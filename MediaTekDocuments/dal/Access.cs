8086b13aeaa024623738b667ff8a67f18f56e844
IsHassaan <94113746+IsHassaan@users.noreply.github.com>
2023-04-27 14:09:04 +0200

    Update Access.cs
    Ajout url d'accès en ligne

--- MediaTekDocuments/dal/Access.cs
+++ MediaTekDocuments/dal/Access.cs
@@ -19,7 +19,7 @@ namespace MediaTekDocuments.dal
         /// <summary>
         /// adresse de l'API
         /// </summary>
-        private static readonly string uriApi = "http://localhost/rest_API/";
+        private static readonly string uriApi = "hokoapp.online";
         /// <summary>
         /// nom de connexion à la bdd
         /// </summary>
